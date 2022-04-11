6406d89b86e26be286b243882a3fa25e60c3aeca
Rob Gentry <97411164+robgentry999@users.noreply.github.com>
2022-04-11 16:52:19 -0500

    added code to make modal

--- fullstack-project-5/student-files/js/script.js
+++ fullstack-project-5/student-files/js/script.js
@@ -1,6 +1,9 @@
 urlAPI = 'https://randomuser.me/api/?results=12';
 const galleryDiv = document.getElementById('gallery');
 let apiInfo = [];
+const body = document.querySelector('body');
+const employeeCard = document.querySelectorAll('.card');
+const modalContainer = document.querySelector('.modal-container');
 
 //gets 12 employees information
 function getJson() {
@@ -35,9 +38,61 @@ function createEmployeeHtml(employeeData) {
         </div>
         `   
     });
-    console.log(employeeHTML);
     galleryDiv.innerHTML = employeeHTML;
 }
 
+//event listener for click
+
+galleryDiv.addEventListener('click', e => {
+    if (e.target !== galleryDiv) {
+        let card = e.target.closest('.card');
+        index = card.getAttribute('data-index');
+        displayModal(index);  
+    }
+});
+
+
+//display modal function
+function displayModal(index) {
+    let { name, dob, phone, email, location: { city, street, state, postcode }, picture } = employees[index];
+    
+    let date = new Date(dob.date);
+
+    const modalHTML = `
+    <div class="modal-container">
+    <div class="modal">
+    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>'
+    <div class="modal-info-container">
+    <img class="modal-img" src="${picture.large}" alt="profile picture">
+    <h3 id="name" class="modal-name cap">${name.first} ${name.last}</h3>
+    <p class="modal-text">${email}</p>
+    <p class="modal-text cap">${city}</p>
+    <hr>
+    <p class="modal-text">${phone}</p>
+    <p class="modal-text">${street.name} ${street.number}, ${state} ${postcode}</p>
+    <p class="modal-text">Birthday: ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+    </div>
+    </div>
+    <div class="modal-btn-container">
+    <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+    <button type="button" id="modal-next" class="modal-next btn">Next</button>
+    </div>
+    `;
+    
+    console.log(modalHTML);
+    body.insertAdjacentHTML('beforeend', modalHTML);
+    body.style.display = 'block';
+
+    const getModal = document.querySelector('.modal-container');
+    const getCloseButton = document.getElementById('modal-close-btn');
+
+    getCloseButton.addEventListener('click', (e) => {
+      const strong = document.querySelector('strong');
+  
+      if (e.target == getCloseButton || strong) {
+        getModal.remove();
+      } 
+    });
+};
 
 getJson();
